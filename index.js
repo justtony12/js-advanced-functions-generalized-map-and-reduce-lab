597126203ba44715e93669db843e7f68726c3888
marcus <marcus.longoria12@gmail.com>
2021-05-19 01:15:27 -0500

    commit tree 22d49fa2ab5a5a761e69a9197a83b6dd417e6b6a

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+function map(src, callback) {
+    const a = [];
+
+    for (let i = 0; i < src.length; i++) {
+        const element = src[i];
+        a.push(callback(element));
+    }
+    return a;
+}
+
+function reduce(src, callback, startingValue) {
+    let total;
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < src.length; i++) {
+            total = callback(src[i], total);
+        }
+        return total;
+    }
+    else {
+        total = src[0];
+        for (let i = 1; i < src.length; i++) {
+            total = callback(src[i], total);
+        }
+        return total;
+    }
+}
